0c8cdd1cc5b51036a9e0127a84a54408cfd673b0
Hiroki Chiyoda <contact@arafipro.com>
2023-12-10 21:52:51 +0900

    フェードインの動画を作成

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,21 @@
+import { AbsoluteFill, interpolate, useCurrentFrame } from "remotion";
+
 export const MyComposition = () => {
-	return null;
+  const frame = useCurrentFrame();
+  const opacity = interpolate(frame, [0, 20], [0, 1]);
+
+  return (
+    <AbsoluteFill
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "7em",
+      }}
+    >
+      <p>{`フレーム数：${frame}`}</p>
+      <div style={{ opacity }}>Hello, World</div>
+    </AbsoluteFill>
+  );
 };
